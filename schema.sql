f8fef8b23ae8d35b2f63358db85f7bde0f557e7d
Joona Kostamo <joona.kostamo@outlook.com>
2022-02-06 21:47:14 +0200

    db fix

--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, 
 CREATE TABLE cats (id SERIAL PRIMARY KEY, topic TEXT);
 INSERT INTO cats (topic) VALUES ('General');
 INSERT INTO cats (topic) VALUES ('Help');
-CREATE TABLE threads (id SERIAL PRIMARY KEY, topic TEXT, category_id INTEGER REFERENCES categories, created_at TIMESTAMP);
+CREATE TABLE threads (id SERIAL PRIMARY KEY, topic TEXT, category_id INTEGER REFERENCES cats, created_at TIMESTAMP);
 CREATE TABLE messages (id SERIAL PRIMARY KEY, content TEXT, user_id INTEGER REFERENCES users, thread_id REFERENCES threads, sent_at TIMESTAMP )
